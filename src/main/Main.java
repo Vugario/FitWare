236c1804f5f83d1a0c7b766039d99bc35918b44b
Allen Laptop <allen@pluxcustoms.nl>
2012-05-09 20:30:45 +0200

    made extra classess

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,12 +7,12 @@ public class Main {
 
 	public static void main(String args[]) {
 
-		//model.User user = new model.User();
+		model.User user = new model.User();
 
 		//Migration.run();
 
-		application = new Application();
-		application.setVisible(true);
+		//application = new Application();
+		//application.setVisible(true);
 		
 	}
 
