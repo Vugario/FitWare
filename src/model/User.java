75894306617a7f9f8131faa40926a95b57e7708b
vm <vm@vm-laptop>
2012-05-09 21:58:46 +0200

    userDataQuery created. V1.1 Made more queries for the Profile page. Still need to be adjusted by Allens great tacticts

--- src/model/User.java
+++ src/model/User.java
@@ -15,22 +15,31 @@ import java.util.logging.Logger;
  * @author allentje
  */
 public class User extends Model {
-	String resultFirstname;
 
-    public User() {
-        //this.query("SELECT * FROM \"user\"");
-       // this.result();
+	String qresultFirstname;
+	String qresultLastname;
+	String qresultSubname;
+	String qresultUsername;
+	String qresultStreet;
+	//String qresultHouseNumber;
+	String qresultCity;
+	String qresultPostcode;
+	String qresultPhonenumber;
 
-    }
+	public User() {
+		//this.query("SELECT * FROM \"user\"");
+		// this.result();
+	}
 
-    public String userDataQuery() {
-        try {
-            this.query("SELECT * FROM \"user\" WHERE id = ?").setInt(1, 1);
+	public String userDataQuery() {
+
+		try {
+			this.query("SELECT * FROM \"user\" WHERE id = ?").setInt(1, 1);
 
 
-        } catch (SQLException ex) {
-            Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
-        }
+		} catch (SQLException ex) {
+			Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
+		}
 		try {
 			this.result = dbQuery.executeQuery();
 		} catch (SQLException ex) {
@@ -39,15 +48,21 @@ public class User extends Model {
 		try {
 
 			while (result.next()) {
-				resultFirstname = result.getString("firstname");
-				System.out.println(resultFirstname);
+				qresultFirstname = result.getString("firstname").toString();
+				qresultLastname = result.getString("lastname").toString();
+				qresultSubname = result.getString("subname").toString();
+				qresultUsername = result.getString("username").toString();userDataQuery
+				qresultStreet = result.getString("street").toString();
+				qresultCity = result.getString("city").toString();
+				
+				
+				
+				System.out.println(qresultFirstname +" \n"+ qresultLastname +" \n"+ qresultSubname +" \n"+ qresultUsername +" \n"+ qresultStreet +" \n"+ qresultCity);
 			}
-		
+
 		} catch (SQLException ex) {
 			Logger.getLogger(Manager.class.getName()).log(Level.SEVERE, null, ex);
 		}
-		return resultFirstname;
-		jTextField2.setText(resultFirstname);
-
-    }
+		return qresultFirstname;
+	}
 }
