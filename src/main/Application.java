efc2a1fdb129e6027ff7c23dbf0025b50426f47a
Daan <Daan@192.168.1.8>
2012-05-17 15:29:23 +0200

    Allen, wat fixes

--- src/main/Application.java
+++ src/main/Application.java
@@ -17,25 +17,24 @@ import javax.swing.UIManager;
 import main.Session;
 import model.User;
 
-
 /**
  *
  * @author allentje
  */
 public final class Application {
-	
-	
+
 	private Manager db;
 	private JFrame window;
-	
 	private static Application instance = new Application();
-	
-	public Application(){
-		
+
+	/**
+	 * A private constructor, to prevent others to make a new instance
+	 */
+	private Application() {
 	}
-	
-	public void initialize(){
-		
+
+	public void initialize() {
+
 		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		} catch (Exception e) {
@@ -43,67 +42,63 @@ public final class Application {
 		}
 
 		startup();
-				
+
 	}
-	
-	public void startup(){
-	
-		
-		
+
+	public void startup() {
+
 		window = new JFrame("Fitware");
 		window.setSize(1000, 600);
 		window.setResizable(false);
 		window.addWindowListener(new WindowAdapter() {
-			
+
 			@Override
-			public void windowClosing(WindowEvent event){
+			public void windowClosing(WindowEvent event) {
 				close();
 			}
-			
 		});
-		
+
 		window.getContentPane().setLayout(new BorderLayout());
-		
+
 		showPanel(new view.Login());
-		
+
 		window.setLocationRelativeTo(null);
 		window.setVisible(true);
 	}
-	
-	public void showPanel(JPanel panel){
-				
+
+	public void showPanel(JPanel panel) {
+
 		JScrollPane scrollpane = new JScrollPane(panel);
 		scrollpane.getVerticalScrollBar().setUnitIncrement(10);
 
 		view.Header header = new view.Header();
 		header.showUserInfo(false);
-		
+
 		window.getContentPane().removeAll();
 		window.getContentPane().add(scrollpane, BorderLayout.CENTER);
 		window.getContentPane().add(header, BorderLayout.NORTH);
-	
+
 		User userSession = Session.get().getLoggedInUser();
 
-		if(userSession == null){
+		if (userSession == null) {
 			window.getContentPane().add(new view.Login());
 
-		}else{
+		} else {
 			header.showUserInfo(true);
 			window.getContentPane().add(new view.menu.MemberMenu(), BorderLayout.WEST);
 			window.getContentPane().add(new view.Header(), BorderLayout.SOUTH);
 		}
-		
+
 		window.getContentPane().validate();
 		window.getContentPane().repaint();
 	}
-	
-	private void close(){
+
+	private void close() {
 		//db.close();
 		window.dispose();
 	}
-	
-	public static Application getInstance(){
+
+	public static Application getInstance() {
 		return instance;
 	}
-	
 }
