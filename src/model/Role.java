9545df601c0f40d056335978129b499d36d29529
Daan <Daan@192.168.1.8>
2012-05-19 14:47:49 +0200

    Created the Role Model

--- src/model/Role.java
+++ src/model/Role.java
@@ -1,15 +1,54 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package model;
 
 import helper.db.Model;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author allentje
  */
 public class Role extends Model{
+	int id;
+	String title;
+	
+	public Role() {	
+	}
+	
+	protected void setPropertiesFromResult() {
+		try {
+
+			// Check if there is a result
+			if (this.result.getRow() == 0) {
+				// There is no result, so return without doing anything
+				return;
+			}
+
+			// Fill in all properties
+			this.id = this.result.getInt("id");
+			this.title = this.result.getString("title");
+			
+			
+		} catch (SQLException ex) {
+			Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
 	
 }
