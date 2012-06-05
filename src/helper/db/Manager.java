76f87a0f7ebe3caff4e0fa1a9afe47d53bbfe70a
vm <vm@vm-laptop.(none)>
2012-06-05 11:16:02 +0200

    Added Javadoc

--- src/helper/db/Manager.java
+++ src/helper/db/Manager.java
@@ -7,17 +7,30 @@ import helper.ExceptionHandler;
 import main.Settings;
 import java.sql.Statement;
 
-
+/**
+ * This is the class to handle the connection to the database
+ * @author Allen
+ */
 public class Manager {
 
+	/**
+	 * dbConnection is the database connection
+	 */
 	private static Connection dbConnection;
-        
+	/**
+	 * Result is the out come of a query
+	 */
 	public ResultSet result;
+	/**
+	 * dbQeury is the query that is sent to the database
+	 */
 	private PreparedStatement dbQuery;
 	private Statement stmnt;
-	
+	/**
+	 * The id of latest row before saving
+	 */
 	private int key;
-	
+
 	public Manager() {
 
 		try {
@@ -29,54 +42,50 @@ public class Manager {
 	}
 
 	public void open() {
-		
+
 		// Check if a connection already exists
 		if (dbConnection != null) {
 			return;
 		}
-		
+
 		try {
-			
+
 			String url = Settings.get("db-url");
 			String name = Settings.get("db-user");
 			String password = Settings.get("db-password");
-			
+
 			dbConnection = DriverManager.getConnection(url, name, password);
 		} catch (SQLException ex) {
 			ExceptionHandler.handle(ex, ExceptionHandler.TYPE_SYSTEM_ERROR);
 		}
 	}
-	
+
 	/**
 	 * prepare all querys
 	 * @param query
 	 * @return The prepared statement 
 	 */
-	public PreparedStatement query(String query)
-	{	
-		
+	public PreparedStatement query(String query) {
+
 		try {
 			dbQuery = dbConnection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
 			//stmnt = dbQuery.//.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-			
+
 		} catch (SQLException ex) {
 			ExceptionHandler.handle(ex, ExceptionHandler.TYPE_SYSTEM_ERROR);
 		}
-		
+
 		return dbQuery;
 	}
-	
-	public static Connection getConnection()
-	{
+
+	public static Connection getConnection() {
 		return dbConnection;
 	}
-	
-	
+
 	/**
 	 * Execute a query returning the last 
 	 */
-	public void execute()
-	{	
+	public void execute() {
 		try {
 			dbQuery.execute();
 
@@ -84,41 +93,40 @@ public class Manager {
 			ExceptionHandler.handle(ex, ExceptionHandler.TYPE_SYSTEM_ERROR);
 		}
 	}
-	
+
 	/**
 	 * gets latest id from table for saving with relations
+	 * @return the latest id from table for saving with relations
 	 */
-	public int getKey(){
-		
+	public int getKey() {
+
 		return key;
 
 	}
 
-	
 	/**
 	 * Execute a query returning a resultset
 	 */
 	public void result() {
-		
+
 		try {
-			
+
 			result = dbQuery.executeQuery();
-		} catch (SQLException ex){
+		} catch (SQLException ex) {
 			ExceptionHandler.handle(ex, ExceptionHandler.TYPE_SYSTEM_ERROR);
 		}
 	}
-	
+
 	/**
-     * remember to close the connection when finished
-     */
-    public void close() {
-        try {
-			if(dbConnection != null) {
-	            dbConnection.close();
+	 * remember to close the connection when finished
+	 */
+	public void close() {
+		try {
+			if (dbConnection != null) {
+				dbConnection.close();
 			}
-        } catch (Exception ex) {
-            ExceptionHandler.handle(ex, ExceptionHandler.TYPE_SYSTEM_ERROR);
-        }
-    }
-	
+		} catch (Exception ex) {
+			ExceptionHandler.handle(ex, ExceptionHandler.TYPE_SYSTEM_ERROR);
+		}
+	}
 }
